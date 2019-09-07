cc0f74c8468e2e250f1c3f8fb7f5e95068302db3
Dwt <dongwentao@dgg.net>
2019-09-07 14:22:21 +0800

    [Update]网络请求路径

---
+++ flutter_shop/lib/config/service_method.dart
@@ -0,0 +1,7 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-09-07 14:13:23
+ * @LastEditTime: 2019-09-07 14:13:37
+ * @LastEditors: Please set LastEditors
+ */
---
+++ flutter_shop/lib/config/service_url.dart
@@ -0,0 +1,7 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-09-07 14:07:29
+ * @LastEditTime: 2019-09-07 14:10:10
+ * @LastEditors: Please set LastEditors
+ */
